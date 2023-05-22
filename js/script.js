290e86b2e511d74d6d3ae41f569f8d0ea30a65da
nico_floriani <floriani.nico@gmail.com>
2023-05-22 11:57:30 -0700

    Added forEach callback function with song and index as parameters, then calling function in button with array as argument

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const mixList = document.querySelector(".mix");
+const button = document.querySelector(".show-list");
+const total = document.querySelector(".total");
+
 const epicMix = [
   "Time to Pretend",
   "O-o-oh Child",
@@ -13,11 +17,19 @@ const epicMix = [
   "Superstition"
 ];
 
-const mixList = document.querySelector(".mix");
-const button = document.querySelector(".show-list");
-const total = document.querySelector(".total");
-
 button.addEventListener("click", function () {
+  mixInfo(epicMix);
   mixList.classList.remove("hide");
   button.classList.add("hide");
 });
+
+total.innerText = `${epicMix.length} great songs!`;
+
+const mixInfo = function (mix) {
+  mix.forEach(function (song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">#${index + 1}</span>${song}`;
+    mixList.append(li);
+  });
+};
